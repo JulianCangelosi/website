243d3fc882730d5f3f5b0c110eb587d4fedf9e26
Julian Cangelosi <jcangelosi20@georgefox.edu>
2024-12-23 18:01:04 -0800

    minor updates

--- pages/home.py
+++ pages/home.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
 st.title('Julian Cangelosi Portfolio')
-
-st.subheader('About me:')
+st.subheader('Welcome to my website!')
+st.write(
+    """Hi there! 👋 I'm a passionate senior in Computer Science, set to graduate in May 2025. With a strong foundation 
+    in coding, problem-solving, and a love for creating impactful solutions, I'm eager to take the next step in my 
+    journey. I'm currently looking for software engineering positions starting on May 19th, 2025. Whether it's building 
+    innovative applications or solving challenging technical problems, I'm excited to bring my skills and enthusiasm to 
+    a dynamic team. Let’s connect and create something amazing together!"""
+)
