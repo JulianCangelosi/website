5583581f343906548efd0325f2693ebd61b183f5
Julian Cangelosi <jcangelosi20@georgefox.edu>
2024-12-23 16:33:08 -0800

    updated sidebar

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ st.title('Julian Cangelosi')
 
 st.button('Hello')
 st.sidebar.title("Sidebar Menu")
+st.sidebar.radio('Choose an option:', ['Option1', 'Option2', 'Option3'])
+st.sidebar.slider('Select a range', 0, 100, 50)
